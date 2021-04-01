bd94584795bff7f40beb6769059c6acdb75a4a16
Nick Hennessy <njhnny@gmail.com>
2021-04-01 02:02:09 -0700

    add vr view

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', onVrViewLoad);
+
+function onVrViewLoad() {
+  // Selector '#vrview' finds element with id 'vrview'.
+  var vrView = new VRView.Player('#vrview', {
+    video: '/url/to/video.mp4',
+    is_stereo: true
+  });
+}
